9458b90d9cf7fdc76ae63bb4c4d256380d6b7856
Oliver Gierke <ogierke@pivotal.io>
2017-07-06 15:57:27 +0200

    #29 - Deploy script now asks for user and password.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 ./render.sh
-find . -type f \( -name "*.html" -o -name "*.png" -o -name "*.gif" -o -name "*.pdf" \) -exec echo {} \; -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASS ftp://static.olivergierke.de/lectures/{} \;
+
+read -s "User: " user
+read -s -p "Password: " password
+
+find . -type f \( -name "*.html" -o -name "*.png" -o -name "*.gif" -o -name "*.pdf" \) -exec echo {} \; -exec curl --ftp-create-dirs -T {} -u $user:$password ftp://static.olivergierke.de/lectures/{} \;
